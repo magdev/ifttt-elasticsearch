b974728f6911f9f4267cb69dd466e3a12b79cb81
magdev <magdev3.0@gmail.com>
2015-03-13 21:25:34 +0100

    delete button works now

--- public/js/app.js
+++ public/js/app.js
@@ -12,4 +12,22 @@
         ev.preventDefault();
         return false;
     });
+    $('.delete').each(function() {
+        $(this).click(function(ev) {
+            ev.preventDefault();
+            if (confirm('Are you sure?')) {
+	            var item = $(this).closest('.item').get(0);
+	            $.post('/delete', item.dataset, function(result) {
+			        $(item).closest('.col').fadeOut({
+			            done: function(result) {
+			                $(this).remove();
+			            },
+			            fail: function(result) {
+                            toast(result.message, 5000, 'rounded');
+                        }
+			        });
+			    });
+			};
+        });
+    });
 })(jQuery);
