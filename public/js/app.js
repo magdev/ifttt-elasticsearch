61630758f4ec99a6dc4bd25fd56f92ecde536ff0
magdev <magdev3.0@gmail.com>
2015-04-05 07:56:03 +0200

    bind event-handlers on new content loaded by infinitescroll

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,35 @@
 (function($) {
-
+    var bindEventHandlers = function(elements) {
+            $(elements).find('.delete').each(function() {
+		        $(this).click(function(ev) {
+		            ev.preventDefault();
+		            if (confirm('Are you sure?')) {
+		                var item = $(this).closest('.item').get(0);
+		                $.post('/delete', item.dataset, function(result) {
+		                    $(item).closest('.col').fadeOut({
+		                        done: function(result) {
+		                            $(this).remove();
+		                        },
+		                        fail: function(result) {
+		                            toast(result.message, 5000, 'rounded');
+		                        }
+		                    });
+		                });
+		            };
+		        });
+		    });
+		    
+		    $(elements).find('.share-button').dropdown({
+		        inDuration: 300,
+		        outDuration: 225,
+		        constrainWidth: false,
+		        hover: false,
+		        alignment: 'right',
+		        gutter: 0,
+		        belowOrigin: true
+		    });
+	    };
+    
 	$('.headroom').click(function(ev) {
 	    $('html, body').animate({
 	        scrollTop: 0
@@ -34,24 +64,7 @@
         ev.preventDefault();
         return false;
     });
-    $('.delete').each(function() {
-        $(this).click(function(ev) {
-            ev.preventDefault();
-            if (confirm('Are you sure?')) {
-	            var item = $(this).closest('.item').get(0);
-	            $.post('/delete', item.dataset, function(result) {
-			        $(item).closest('.col').fadeOut({
-			            done: function(result) {
-			                $(this).remove();
-			            },
-			            fail: function(result) {
-                            toast(result.message, 5000, 'rounded');
-                        }
-			        });
-			    });
-			};
-        });
-    });
+    bindEventHandlers($('.card'));
     $('.infsc-container').infinitescroll({
         navSelector: '.pagination',
         nextSelector: '.paginator-next',
@@ -59,19 +72,12 @@
         donetext: '',
         loadingText: '',
         loadingImg: ''
+    }, function(newElements) {
+        bindEventHandlers(newElements);
     });
     $('.btn-add').click(function(ev) {
         ev.preventDefault();
         window.open('/push', 'indexall', 'locationbar=yes,width=480px,height=500px,statusbar=no,menubar=no,scrollbars=yes,toolbar=no,resizable=yes')
-    });
-    $('.share-button').dropdown({
-        inDuration: 300,
-        outDuration: 225,
-        constrainWidth: false,
-	    hover: false,
-	    alignment: 'right',
-	    gutter: 0,
-	    belowOrigin: true
     });
     var shr = new Share();
     $('.slider').slider({
